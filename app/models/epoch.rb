75f925a381e96d261dc66f26e79337dc016b4c61
Florian <florian.eppel@7fun.de>
2021-05-12 22:41:15 +0200

    added spectrum view for epochs

--- app/models/epoch.rb
+++ app/models/epoch.rb
@@ -1,3 +1,7 @@
 class Epoch < ApplicationRecord
 	has_many :results
+
+	has_many :frequencies, :through => :results
+	has_many :sources, :through => :results
 end
+
--- app/models/frequency.rb
+++ app/models/frequency.rb
@@ -1,3 +1,7 @@
 class Frequency < ApplicationRecord
 	has_many :results
+
+	has_many :sources, :through => :results
+	has_many :epoches, :through => :results
 end
+
