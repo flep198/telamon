4279127a37c1681547c999903a71cabad4ac6632
Florian <florian.eppel@7fun.de>
2021-05-14 23:29:59 +0200

    Minor Changes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= require jquery.turbolinks
 //= require_tree .
-//= require turbolinks
 //= require_self
+//= require jquery.turbolinks
+//= require turbolinks
