b5dba2c68810b8ab4581993fde76f2c15e645cd0
Florian <florian.eppel@7fun.de>
2021-07-26 15:49:56 +0200

    Added Spectral Index Plot

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'devise'
 gem 'jquery-turbolinks'
 gem 'friendly_id', '~> 5.4', '>= 5.4.2'
 gem 'plotly-js', '~> 1.20', '>= 1.20.5.4'
+gem 'linefit', '~> 0.3.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -1,2 +1,37 @@
 module SourcesHelper
+	def getSpectralIndices source
+		slopes = Array.new
+		slope_errs = Array.new
+		mjds = Array.new
+
+		source.epoches.distinct.order(:date).each do |epoch|
+    		
+    		#Import Data
+    		@epoch_id=epoch.id
+    		@data = Result.where(:source_id => @source.id, :epoch_id => @epoch_id).map { |r| [Frequency.find(r.frequency_id).freq_ghz,r.value_jy,r.error_jy,r.mjd]}
+    		@data = @data.sort_by(&:first)
+
+    		x = @data.map{|r| Math.log(r[0])}
+  			y = @data.map{|r| Math.log(r[1])}
+  			yerr = @data.map{|r| r[2]/r[1]}
+  			mjd = @data.map{|r| r[3]}
+  			mjd_aver= mjd.inject{ |sum, el| sum + el }.to_f / mjd.size #calculate Average MJD
+
+  			if(x.size >1)
+  				#do linear fit
+  				lineFit = LineFit.new
+  				lineFit.setData(x,y,yerr)
+
+  				slope = lineFit.coefficients[1]
+  				slope_err = Math.sqrt(lineFit.varianceOfEstimates[1].abs) #CHECK IF THIS IS THE RIGHT THING TO DO!!!!!
+
+  				#Push new values to array
+  				slopes.push(slope)
+  				slope_errs.push(slope_err)
+  				mjds.push(mjd_aver)
+  			end
+    	end
+
+    	return mjds, slopes, slope_errs
+    end
 end
