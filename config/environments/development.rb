e69a98908ee1d8fdce13c7f6cf4c3ecad0bcd36c
Florian <florian.eppel@7fun.de>
2021-09-10 14:00:58 +0200

    Lockup design changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  ENV['LOCKUP_CODEWORD'] = 'secret'
+  config.hosts << "telamon.astro.uni-wuerzburg.de"
+  ENV['LOCKUP_CODEWORD'] = 'radiovariability'
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
+  config.hosts << "telamon.astro.uni-wuerzburg.de"
   ENV['LOCKUP_CODEWORD'] = 'radiovariability'
 
 end
