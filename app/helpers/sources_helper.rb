d07571077359336e486e5886b0d86b6bd64896e5
Florian <florian.eppel@7fun.de>
2021-09-27 23:34:45 +0200

    Error fixed in spectral fit

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -17,7 +17,7 @@ module SourcesHelper
   			mjd = @data.map{|r| r[3]}
   			mjd_aver= mjd.inject{ |sum, el| sum + el }.to_f / mjd.size #calculate Average MJD
 
-  			if(x.size >1)
+  			if(x.size >2)
   				#do linear fit
   				lineFit = LineFit.new
   				lineFit.setData(x,y,yerr)
