fba6977b981137397cca5551339c330b32ecbd60
Florian <florian.eppel@7fun.de>
2022-01-27 22:47:12 +0100

    made progress in filtering table, new method updateTable() in js

--- db/seeds/source_categories.rb
+++ db/seeds/source_categories.rb
@@ -1,5 +1,7 @@
 @a=SourceCategory.all.where(name: "Sample I").first_or_create
+@a.update(name: "Sample 1")
 @b=SourceCategory.all.where(name: "Sample II").first_or_create
+@b.update(name: "Sample 2")
 @c=SourceCategory.all.where(name: "Calibrator").first_or_create
 @d=SourceCategory.all.where(name: "Neutrino").first_or_create
 @e=SourceCategory.all.where(name: "Bad Weather").first_or_create
