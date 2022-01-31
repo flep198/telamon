06de389a5b58ac217871543979c529bf4d246b80
Florian <florian.eppel@7fun.de>
2022-01-31 21:32:35 +0100

    table filter finalized, user roles added (admin,user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: {user: 0, admin: 1}
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_124418) do
+ActiveRecord::Schema.define(version: 2022_01_31_140235) do
 
   create_table "epoches", force: :cascade do |t|
     t.date "date"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_124418) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
