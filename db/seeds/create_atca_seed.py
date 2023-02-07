b0b660978f6cccbd3adedb5eae058eb2667439ee
Florian <florian.eppel@7fun.de>
2023-02-07 19:13:35 +0100

    seed updated

--- db/seeds/create_atca_seed.py
+++ db/seeds/create_atca_seed.py
@@ -23,7 +23,7 @@ with open('atca_seeds.rb','w') as f:
         if telamon_name=="0739+0137":
             telamon_name="0739+0136"
         if telamon_name=="2131-1207":
-            telamon_naem="2128-123"
+            telamon_name="2128-123"
         if telamon_name=="0521+1638":
             telamon_name="3C138"
         if telamon_name=="0627-0553":
@@ -34,6 +34,10 @@ with open('atca_seeds.rb','w') as f:
             telamon_name="3C286"
         if telamon_name=="0242-0000":
             telamon_name="NGC1068"
+        if telamon_name=="0339-0146":
+            telamon_name="0336-019"
+        if telamon_name=="2225-0457":
+            telamon_name="3C446"
         print("@a"+telamon_name.replace("+","").replace("-","")+"=Source.where(j2000_name: '"+telamon_name+"').first_or_create")
         print("@a"+telamon_name.replace("+","").replace("-","")+".update(atca_url: 'https://www.narrabri.atnf.csiro.au/calibrators/calibrator_database_viewcal.html?source="+row['Source'] + "&detailed=true')")
         print("AtcaResult.create(value_jy: "+str(row['Flux Density [Jy]'])+
