76bd55afc70c76069825fe5756e5aed861c84c28
Florian <florian.eppel@7fun.de>
2023-02-07 17:14:47 +0100

    seed updated

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,15 +1,15 @@
 module HomeHelper
 
 	def getNscans febe_name
-		mjd_prev=0
+		scan_prev=0
 		scan_count=0
 		febe_prev=""
 		observations=Observation.all.order("mjd").each do |obs|
 			
-			if (obs.mjd!=mjd_prev or febe_prev!=febe_name) && obs.febe==febe_name
+			if (obs.scan_nr!=scan_prev or febe_prev!=febe_name) && obs.febe==febe_name
 				scan_count=scan_count+obs.n_scans
 			end
-			mjd_prev=obs.mjd
+			scan_prev=obs.scan_nr
 			febe_prev=obs.febe
 
 		end
